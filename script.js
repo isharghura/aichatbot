1945e0ee34868a1836262fac20e56d6c586dc156
isharghura <ishar.ghuras@gmail.com>
2023-06-21 15:04:41 -0400

    allowed bot to respond

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
-const userMsg = document.getElementById("userText");
-const usermsgs = document.getElementById("msgs");
+const userInput = document.getElementById("userText");
+const msgs = document.getElementById("msgs");
+let count = 0;
 
 function addMsg() {
-    if (userMsg.value === '') {
+    if (userInput.value === '') {
         //do nothing
     }
     else {
         let usrmsg = document.createElement("li");
         usrmsg.id = "user-msg";
+        let botmsg = document.createElement("li");
+        botmsg.id = "bot-msg";
 
-        usrmsg.innerHTML = userMsg.value;
+        usrmsg.innerHTML = userInput.value;
+        botmsg.innerHTML = "blah";
 
-        usermsgs.appendChild(usrmsg);
-        userMsg.value = '';
+        msgs.appendChild(usrmsg);
+        userInput.value = '';
+        if (count === 5) {
+            msgs.appendChild(botmsg);
+        }
     }
 }
 
 document.addEventListener('keydown', (event) => {
     var whichKey = event.key;
     if (whichKey === 'Enter') {
+        count++;
         event.preventDefault();
         addMsg();
+
         var boxElement = document.querySelector('.box');
         boxElement.scrollTop = boxElement.scrollHeight;
-        userText.focus();
+        userInput.focus();
     }
 })
