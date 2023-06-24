07c8ba7ba684a39ed2693cf328a6a65460221b30
isharghura <ishar.ghuras@gmail.com>
2023-06-23 23:12:50 -0400

    made venv

--- training.py
+++ training.py
@@ -1,31 +1,30 @@
-import random
-import json
-import pickle
-import numpy as np
+# # import random
+# # import json
+# # import pickle
+# # import numpy as np
 
-import nltk
+# # import nltk
 
-from nltk.stem import WordNetLemmatizer
+# # from nltk.stem import WordNetLemmatizer
 
-from tensorflow import keras
-from keras.models import Sequential
-from keras.layers import Dense, Activation, Dropout
-from keras.optimizers import SGD
+# # from tensorflow.keras.models import Sequential
+# # from tensorflow.keras.layers import Dense, Activation, Dropout
+# # from tensorflow.keras.optimizers import SGD
 
-lemmatizer = WordNetLemmatizer
-intents = json.load(open('intents.json').read())
+# lemmatizer = WordNetLemmatizer
+# intents = json.load(open('intents.json').read())
 
-words = []
-classes = []
-documents = []
-ignore_letters = ['?', '!', '.', ',']
+# words = []
+# classes = []
+# documents = []
+# ignore_letters = ['?', '!', '.', ',']
 
-for intent in intents['intents']:
-    for pattern in intent['patterns']:
-        word_list=nltk.word_tokenize(pattern)
-        words.append(word_list)
-        documents.append((word_list), intent['tag'])
-        if intent['tag'] not in classes:
-            classes.append(intent['tag'])
+# for intent in intents['intents']:
+#     for pattern in intent['patterns']:
+#         word_list=nltk.word_tokenize(pattern)
+#         words.append(word_list)
+#         documents.append((word_list), intent['tag'])
+#         if intent['tag'] not in classes:
+#             classes.append(intent['tag'])
 
-print(documents)
+# print(documents)
