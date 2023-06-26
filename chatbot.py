961ca4bd931fbde54893d2cba0eb31da1cbe5ed2
isharghura <ishar.ghuras@gmail.com>
2023-06-26 17:19:03 -0400

    updated chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -14,7 +14,7 @@ intents = json.loads(open('intents.json').read())
 
 words = pickle.load(open('words.pkl', 'rb'))
 classes = pickle.load(open('classes.pkl', 'rb'))
-model = load_model('chatbot_model.model')
+model = load_model('chatbotmodel.h5')
 
 def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
@@ -29,3 +29,32 @@ def bag_of_words(sentence):
             if word == w:
                 bag[i] = 1
     return np.array(bag)
+
+def predict_class(sentence):
+    bow = bag_of_words(sentence)
+    res = model.predict(np.array([bow]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+    return return_list
+
+def get_response(intents_list, intents_json):
+    tag = intents_list[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if i in list_of_intents:
+            result = random.choice(i['responses'])
+            break
+    return result
+
+print("Running!")
+
+while True: 
+    message=input("Type here: ")
+    ints=predict_class(message)
+    res = get_response(ints, intents)
+    print(res)
