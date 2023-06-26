0593f1f66b3933ba4f329a07b4abd4782c4ea112
isharghura <ishar.ghuras@gmail.com>
2023-06-26 13:21:06 -0400

    built neural network

--- training.py
+++ training.py
@@ -8,9 +8,10 @@ import nltk
 nltk.download("wordnet")
 from nltk.stem import WordNetLemmatizer
 
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Activation, Dropout
-from tensorflow.keras.optimizers import SGD
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizers import SGD
 
 lemmatizer = WordNetLemmatizer()
 
@@ -56,3 +57,17 @@ training=np.array(training)
 
 train_x=list(training[:, 0])
 train_y=list(training[:, 1])
+
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.model')
+print("Done")
