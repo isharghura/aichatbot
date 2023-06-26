76872661bb99940c16c6be564853bf36feb2a65b
isharghura <ishar.ghuras@gmail.com>
2023-06-26 19:51:12 -0400

    confirmed ajax request works

--- app.py
+++ app.py
@@ -3,13 +3,16 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 
-# @app.route("/")
-# def index():
-#     return render_template("index.html")
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 @app.route('/process', methods=['POST'])
 def process():
-    input_data = request.form.get('inputVariable')
+    input_data = request.form.get('input_variable')
+    if input_data is None:
+        return jsonify({'error': 'No input data provided'})
+
     response = 'Response message based on input: ' + input_data
     return jsonify({'response': response})
 
