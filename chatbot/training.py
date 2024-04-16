ba727d987d380c8f5e3d214366c7c3cb2cf67a44
isharghura <ishar.ghuras@gmail.com>
2024-04-15 20:05:47 -0400

    Added documentation to training.py

--- chatbot/training.py
+++ chatbot/training.py
@@ -23,6 +23,7 @@ classes = []
 documents = []
 ignore_letters = ["?", "!", ".", ","]
 
+# Categorizes patterns from each intent into tags
 for intent in intents["intents"]:
     for pattern in intent["patterns"]:
         word_list = nltk.word_tokenize(pattern)
@@ -31,17 +32,22 @@ for intent in intents["intents"]:
         if intent["tag"] not in classes:
             classes.append(intent["tag"])
 
+# Stems each word
 words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
 words = sorted(set(words))
 
+# Gets rid of duplicates
 classes = sorted(set(classes))
 
+# Serializes words and classes
 pickle.dump(words, open("words.pkl", "wb"))
 pickle.dump(classes, open("classes.pkl", "wb"))
 
+# Template for target output during creation of the training data
 training = []
 output_empty = [0] * len(classes)
 
+# Prepares training data, converts patterns into numerical representation
 for document in documents:
     bag = []
     word_patterns = document[0]
@@ -53,12 +59,15 @@ for document in documents:
     output_row[classes.index(document[1])] = 1
     training.append([bag, output_row])
 
+# Shuffle training data, convert into NumPy array
 random.shuffle(training)
 training = np.array(training)
 
+# Separates training data into input features and target labels
 train_x = list(training[:, 0])
 train_y = list(training[:, 1])
 
+# Defines the architecture of the nueral network model with Keras
 model = Sequential()
 model.add(Dense(128, input_shape=(len(train_x[0]),), activation="relu"))
 model.add(Dropout(0.5))
@@ -66,11 +75,15 @@ model.add(Dense(64, activation="relu"))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation="softmax"))
 
+# Configures the model for training
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss="categorical_crossentropy", optimizer=sgd, metrics=["accuracy"])
 
+# Trains the compiled model
 hist = model.fit(
     np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1
 )
+
+# Saves the model into a file and confirms that training is finished
 model.save("chatbotmodel.h5", hist)
 print("Finished training!")
